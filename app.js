963eb33ceaab0fa85e84515e4fa130f8ba34d610
pavankudipudi <121846255+pavankudipudi@users.noreply.github.com>
2023-03-19 20:00:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(' i am wroking with vinay,nagendra and pavan jkj;lkj !\n');
+  res.end(' i am wroking with vinay,nagendra and pavan !\n');
 });
 
 server.listen(port, hostname, () => {
